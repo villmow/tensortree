79fa4ed91241473930e09daf2be54821c95c3de5
Johannes Villmow <johannes.villmow@hs-rm.de>
2021-06-30 17:26:29 +0200

    fix license

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',  # Define that your audience are developers
         'Topic :: Scientific/Engineering',
-        'License :: OSI Approved :: MIT Software License',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
 )
